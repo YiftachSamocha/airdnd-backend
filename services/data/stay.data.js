76162ba44833b98ec1766610748689c1e079bc29
Yiftach Samocha <yiftachsamo@gmail.com>
2024-09-19 22:30:12 +0300

    Fix amenities data

--- services/data/stay.data.js
+++ services/data/stay.data.js
@@ -66,7 +66,7 @@ const parkingImg = '../../assets/imgs/Extra/parking.png'
 const tvImg = '../../assets/imgs/Extra/tv.png'
 const acImg = '../../assets/imgs/Extra/ac.png'
 const kitchenImg = '../../assets/imgs/Extra/kitchen.png'
-const washingMashineImg = '/src/assets/imgs/more/washer.svg'
+const washingMashineImg = 'https://res.cloudinary.com/dfacuc12l/image/upload/washer.svg'
 const wifiImg = '../../assets/imgs/Extra/wifi.png'
 
 export function createStayData(users, listingsPerHost = 4) {
@@ -375,7 +375,7 @@ const highlights = [
     {
         main: 'Flexible cancellation policy',
         sub: 'Get a full refund if you cancel within 48 hours of booking.',
-        imgUrl: '/src/assets/imgs/more/safe.svg'
+        imgUrl: 'https://res.cloudinary.com/dfacuc12l/image/upload/safe.svg'
 
     },
     {
@@ -459,7 +459,7 @@ const highlights = [
     {
         main: 'Free parking on premises',
         sub: 'This place offers free parking for added convenience.',
-        imgUrl: '/src/assets/imgs/more/parking.svg'
+        imgUrl: 'https://res.cloudinary.com/dfacuc12l/image/upload/parking2.svg'
 
     },
     {
@@ -537,104 +537,105 @@ const highlights = [
 
 ]
 const types = ['home', 'room', 'apartment', 'villa',]
+
 const amenities = [
-    { type: 'main', name: 'wifi', imgUrl: '/src/assets/imgs/amenities/no-wifi.svg' },
-    { type: 'main', name: 'Air conditioning', imgUrl: '/src/assets/imgs/more/aircon.svg' },
-    { type: 'main', name: 'kitchen', imgUrl: '/src/assets/imgs/more/kitchen1.svg' },
-    { type: 'main', name: 'free parking', imgUrl: '/src/assets/imgs/more/parking.svg' },
-    { type: 'main', name: 'Television', imgUrl: '/src/assets/imgs/Extra/tv.png' },
-    { type: 'main', name: 'washing mashine', imgUrl: '/src/assets/imgs/more/washer.svg' },
+    { type: 'main', name: 'wifi', imgUrl: 'https://res.cloudinary.com/dfacuc12l/image/upload/no-wifi.svg' },
+    { type: 'main', name: 'Air conditioning', imgUrl: 'https://res.cloudinary.com/dfacuc12l/image/upload/aircon.svg' },
+    { type: 'main', name: 'kitchen', imgUrl: 'https://res.cloudinary.com/dfacuc12l/image/upload/kitchen1.svg' },
+    { type: 'main', name: 'free parking', imgUrl: 'https://res.cloudinary.com/dfacuc12l/image/upload/parking2.svg' },
+    { type: 'main', name: 'Television', imgUrl: 'https://res.cloudinary.com/dfacuc12l/image/upload/tv.png' },
+    { type: 'main', name: 'washing mashine', imgUrl: 'https://res.cloudinary.com/dfacuc12l/image/upload/washer.svg' },
 
-    { type: 'bathroom', name: 'Hair dryer', imgUrl: '/src/assets/imgs/more/hair-dryer.svg' },
-    { type: 'bathroom', name: 'Shampoo', imgUrl: '/src/assets/imgs/amenities/essentials.svg' },
-    { type: 'bathroom', name: 'Conditioner', imgUrl: '/src/assets/imgs/amenities/essentials.svg' },
-    { type: 'bathroom', name: 'Body soap', imgUrl: '/src/assets/imgs/more/body-saop.svg' },
-    { type: 'bathroom', name: 'Hot water', imgUrl: '/src/assets/imgs/amenities/hot-water.svg' },
-    { type: 'bathroom', name: 'Towels', imgUrl: '/src/assets/imgs/amenities/hot-water.svg' },
-    { type: 'bathroom', name: 'Toilet paper', imgUrl: '/src/assets/imgs/amenities/hot-water.svg' },
-    { type: 'bathroom', name: 'Bathrobe', imgUrl: '/src/assets/imgs/amenities/hot-water.svg' },
-    { type: 'bathroom', name: 'Slippers', imgUrl: '/src/assets/imgs/amenities/hot-water.svg' },
+    { type: 'bathroom', name: 'Hair dryer', imgUrl: 'https://res.cloudinary.com/dfacuc12l/image/upload/hair-dryer.svg' },
+    { type: 'bathroom', name: 'Shampoo', imgUrl: 'https://res.cloudinary.com/dfacuc12l/image/upload/essentials.svg' },
+    { type: 'bathroom', name: 'Conditioner', imgUrl: 'https://res.cloudinary.com/dfacuc12l/image/upload/essentials.svg' },
+    { type: 'bathroom', name: 'Body soap', imgUrl: 'https://res.cloudinary.com/dfacuc12l/image/upload/body-saop.svg' },
+    { type: 'bathroom', name: 'Hot water', imgUrl: 'https://res.cloudinary.com/dfacuc12l/image/upload/hot-water.svg' },
+    { type: 'bathroom', name: 'Towels', imgUrl: 'https://res.cloudinary.com/dfacuc12l/image/upload/hot-water.svg' },
+    { type: 'bathroom', name: 'Toilet paper', imgUrl: 'https://res.cloudinary.com/dfacuc12l/image/upload/hot-water.svg' },
+    { type: 'bathroom', name: 'Bathrobe', imgUrl: 'https://res.cloudinary.com/dfacuc12l/image/upload/hot-water.svg' },
+    { type: 'bathroom', name: 'Slippers', imgUrl: 'https://res.cloudinary.com/dfacuc12l/image/upload/hot-water.svg' },
 
-    { type: 'heatingAndCooling', name: 'Air conditioning', imgUrl: '/src/assets/imgs/more/aircon.svg' },
-    { type: 'heatingAndCooling', name: 'Central heating', imgUrl: '/src/assets/imgs/amenities/indoor-fireplace.svg' },
-    { type: 'heatingAndCooling', name: 'Portable fans', imgUrl: '/src/assets/imgs/more/fan.svg' },
-    { type: 'heatingAndCooling', name: 'Space heater', imgUrl: '/src/assets/imgs/more/hot.svg' },
-    { type: 'heatingAndCooling', name: 'Dehumidifier', imgUrl: '/src/assets/imgs/amenities/indoor-fireplace.svg' },
+    { type: 'heatingAndCooling', name: 'Air conditioning', imgUrl: 'https://res.cloudinary.com/dfacuc12l/image/upload/aircon.svg' },
+    { type: 'heatingAndCooling', name: 'Central heating', imgUrl: 'https://res.cloudinary.com/dfacuc12l/image/upload/indoor-fireplace.svg' },
+    { type: 'heatingAndCooling', name: 'Portable fans', imgUrl: 'https://res.cloudinary.com/dfacuc12l/image/upload/fan.svg' },
+    { type: 'heatingAndCooling', name: 'Space heater', imgUrl: 'https://res.cloudinary.com/dfacuc12l/image/upload/hot.svg' },
+    { type: 'heatingAndCooling', name: 'Dehumidifier', imgUrl: 'https://res.cloudinary.com/dfacuc12l/image/upload/indoor-fireplace.svg' },
 
-    { type: 'kitchen', name: 'Refrigerator', imgUrl: '/src/assets/imgs/more/refrigirator.svg' },
-    { type: 'kitchen', name: 'Microwave', imgUrl: '/src/assets/imgs/more/microwave.svg' },
-    { type: 'kitchen', name: 'Oven', imgUrl: '/src/assets/imgs/more/oven.svg' },
-    { type: 'kitchen', name: 'Stove', imgUrl: '/src/assets/imgs/more/oven.svg' },
-    { type: 'kitchen', name: 'Dishwasher', imgUrl: '/src/assets/imgs/more/dishwasher.svg' },
-    { type: 'kitchen', name: 'Coffee maker', imgUrl: '/src/assets/imgs/more/cofee.svg' },
-    { type: 'kitchen', name: 'Toaster', imgUrl: '/src/assets/imgs/more/toast.svg' },
-    { type: 'kitchen', name: 'Cooking basics', imgUrl: '/src/assets/imgs/amenities/breakfast.svg' },
-    { type: 'kitchen', name: 'Dishes and silverware', imgUrl: '/src/assets/imgs/more/dishes.svg' },
-    { type: 'kitchen', name: 'Wine glasses', imgUrl: '/src/assets/imgs/more/wine.svg' },
+    { type: 'kitchen', name: 'Refrigerator', imgUrl: 'https://res.cloudinary.com/dfacuc12l/image/upload/refrigirator.svg' },
+    { type: 'kitchen', name: 'Microwave', imgUrl: 'https://res.cloudinary.com/dfacuc12l/image/upload/microwave.svg' },
+    { type: 'kitchen', name: 'Oven', imgUrl: 'https://res.cloudinary.com/dfacuc12l/image/upload/oven.svg' },
+    { type: 'kitchen', name: 'Stove', imgUrl: 'https://res.cloudinary.com/dfacuc12l/image/upload/oven.svg' },
+    { type: 'kitchen', name: 'Dishwasher', imgUrl: 'https://res.cloudinary.com/dfacuc12l/image/upload/dishwasher.svg' },
+    { type: 'kitchen', name: 'Coffee maker', imgUrl: 'https://res.cloudinary.com/dfacuc12l/image/upload/cofee.svg' },
+    { type: 'kitchen', name: 'Toaster', imgUrl: 'https://res.cloudinary.com/dfacuc12l/image/upload/toast.svg' },
+    { type: 'kitchen', name: 'Cooking basics', imgUrl: 'https://res.cloudinary.com/dfacuc12l/image/upload/breakfast.svg' },
+    { type: 'kitchen', name: 'Dishes and silverware', imgUrl: 'https://res.cloudinary.com/dfacuc12l/image/upload/dishes.svg' },
+    { type: 'kitchen', name: 'Wine glasses', imgUrl: 'https://res.cloudinary.com/dfacuc12l/image/upload/wine.svg' },
 
-    { type: 'livingRoom', name: 'TV', imgUrl: '/src/assets/imgs/Extra/tv.png' },
-    { type: 'livingRoom', name: 'Cable TV', imgUrl: '/src/assets/imgs/Extra/tv.png' },
-    { type: 'livingRoom', name: 'Streaming services', imgUrl: '/src/assets/imgs/amenities/host-greets-you.svg' },
-    { type: 'livingRoom', name: 'DVD player', imgUrl: '/src/assets/imgs/amenities/host-greets-you.svg' },
-    { type: 'livingRoom', name: 'Sofa', imgUrl: '/src/assets/imgs/amenities/host-greets-you.svg' },
-    { type: 'livingRoom', name: 'Coffee table', imgUrl: '/src/assets/imgs/more/cofee.svg' },
-    { type: 'livingRoom', name: 'Books and reading material', imgUrl: '/src/assets/imgs/more/book.svg' },
+    { type: 'livingRoom', name: 'TV', imgUrl: 'https://res.cloudinary.com/dfacuc12l/image/upload/tv.png' },
+    { type: 'livingRoom', name: 'Cable TV', imgUrl: 'https://res.cloudinary.com/dfacuc12l/image/upload/tv.png' },
+    { type: 'livingRoom', name: 'Streaming services', imgUrl: 'https://res.cloudinary.com/dfacuc12l/image/upload/host-greets-you.svg' },
+    { type: 'livingRoom', name: 'DVD player', imgUrl: 'https://res.cloudinary.com/dfacuc12l/image/upload/host-greets-you.svg' },
+    { type: 'livingRoom', name: 'Sofa', imgUrl: 'https://res.cloudinary.com/dfacuc12l/image/upload/host-greets-you.svg' },
+    { type: 'livingRoom', name: 'Coffee table', imgUrl: 'https://res.cloudinary.com/dfacuc12l/image/upload/cofee.svg' },
+    { type: 'livingRoom', name: 'Books and reading material', imgUrl: 'https://res.cloudinary.com/dfacuc12l/image/upload/book.svg' },
 
-    { type: 'outdoor', name: 'Patio or balcony', imgUrl: '/src/assets/imgs/amenities/patio.svg' },
-    { type: 'outdoor', name: 'Garden', imgUrl: '/src/assets/imgs/amenities/backyard.svg' },
-    { type: 'outdoor', name: 'BBQ grill', imgUrl: '/src/assets/imgs/amenities/bbq-gril.svg' },
-    { type: 'outdoor', name: 'Outdoor furniture', imgUrl: '/src/assets/imgs/amenities/outdoor-dining.svg' },
-    { type: 'outdoor', name: 'Fire pit', imgUrl: '/src/assets/imgs/amenities/fire-pit.svg' },
-    { type: 'outdoor', name: 'Pool', imgUrl: '/src/assets/imgs/amenities/pool.svg' },
-    { type: 'outdoor', name: 'Hot tub', imgUrl: '/src/assets/imgs/amenities/hot-tub.svg' },
+    { type: 'outdoor', name: 'Patio or balcony', imgUrl: 'https://res.cloudinary.com/dfacuc12l/image/upload/patio.svg' },
+    { type: 'outdoor', name: 'Garden', imgUrl: 'https://res.cloudinary.com/dfacuc12l/image/upload/backyard.svg' },
+    { type: 'outdoor', name: 'BBQ grill', imgUrl: 'https://res.cloudinary.com/dfacuc12l/image/upload/bbq-gril.svg' },
+    { type: 'outdoor', name: 'Outdoor furniture', imgUrl: 'https://res.cloudinary.com/dfacuc12l/image/upload/outdoor-dining.svg' },
+    { type: 'outdoor', name: 'Fire pit', imgUrl: 'https://res.cloudinary.com/dfacuc12l/image/upload/fire-pit.svg' },
+    { type: 'outdoor', name: 'Pool', imgUrl: 'https://res.cloudinary.com/dfacuc12l/image/upload/pool.svg' },
+    { type: 'outdoor', name: 'Hot tub', imgUrl: 'https://res.cloudinary.com/dfacuc12l/image/upload/hot-tub.svg' },
 
-    { type: 'safety', name: 'Smoke alarm', imgUrl: '/src/assets/imgs/amenities/smoke-alarm.svg' },
-    { type: 'safety', name: 'Carbon monoxide alarm', imgUrl: '/src/assets/imgs/amenities/carbon-alarm.svg' },
-    { type: 'safety', name: 'First aid kit', imgUrl: '/src/assets/imgs/amenities/first-aid-kit.svg' },
-    { type: 'safety', name: 'Fire extinguisher', imgUrl: '/src/assets/imgs/amenities/fire-extinguisher.svg' },
-    { type: 'safety', name: 'Lock on bedroom door', imgUrl: '/src/assets/imgs/amenities/queen-bed.svg' },
+    { type: 'safety', name: 'Smoke alarm', imgUrl: 'https://res.cloudinary.com/dfacuc12l/image/upload/smoke-alarm.svg' },
+    { type: 'safety', name: 'Carbon monoxide alarm', imgUrl: 'https://res.cloudinary.com/dfacuc12l/image/upload/carbon-alarm.svg' },
+    { type: 'safety', name: 'First aid kit', imgUrl: 'https://res.cloudinary.com/dfacuc12l/image/upload/first-aid-kit.svg' },
+    { type: 'safety', name: 'Fire extinguisher', imgUrl: 'https://res.cloudinary.com/dfacuc12l/image/upload/fire-extinguisher.svg' },
+    { type: 'safety', name: 'Lock on bedroom door', imgUrl: 'https://res.cloudinary.com/dfacuc12l/image/upload/queen-bed.svg' },
 
-    { type: 'workspace', name: 'Desk', imgUrl: '/src/assets/imgs/amenities/workspace.svg' },
-    { type: 'workspace', name: 'Laptop - friendly workspace', imgUrl: '/src/assets/imgs/amenities/workspace.svg' },
-    { type: 'workspace', name: 'Office chair', imgUrl: '/src/assets/imgs/amenities/workspace.svg' },
-    { type: 'workspace', name: 'Printer', imgUrl: '/src/assets/imgs/amenities/workspace.svg' },
-    { type: 'workspace', name: 'High-speed internet', imgUrl: '/src/assets/imgs/amenities/workspace.svg' },
+    { type: 'workspace', name: 'Desk', imgUrl: 'https://res.cloudinary.com/dfacuc12l/image/upload/workspace.svg' },
+    { type: 'workspace', name: 'Laptop - friendly workspace', imgUrl: 'https://res.cloudinary.com/dfacuc12l/image/upload/workspace.svg' },
+    { type: 'workspace', name: 'Office chair', imgUrl: 'https://res.cloudinary.com/dfacuc12l/image/upload/workspace.svg' },
+    { type: 'workspace', name: 'Printer', imgUrl: 'https://res.cloudinary.com/dfacuc12l/image/upload/workspace.svg' },
+    { type: 'workspace', name: 'High-speed internet', imgUrl: 'https://res.cloudinary.com/dfacuc12l/image/upload/workspace.svg' },
 
-    { type: 'accessibility', name: 'Wheelchair accessible', imgUrl: '/src/assets/imgs/amenities/free-parking.svg' },
-    { type: 'accessibility', name: 'Elevator', imgUrl: '/src/assets/imgs/amenities/free-parking.svg' },
-    { type: 'accessibility', name: 'Accessible entrance', imgUrl: '/src/assets/imgs/amenities/free-parking.svg' },
-    { type: 'accessibility', name: 'Accessible bathroom', imgUrl: '/src/assets/imgs/amenities/free-parking.svg' },
-    { type: 'accessibility', name: 'Grab rails', imgUrl: '/src/assets/imgs/amenities/free-parking.svg' },
+    { type: 'accessibility', name: 'Wheelchair accessible', imgUrl: 'https://res.cloudinary.com/dfacuc12l/image/upload/free-parking.svg' },
+    { type: 'accessibility', name: 'Elevator', imgUrl: 'https://res.cloudinary.com/dfacuc12l/image/upload/free-parking.svg' },
+    { type: 'accessibility', name: 'Accessible entrance', imgUrl: 'https://res.cloudinary.com/dfacuc12l/image/upload/free-parking.svg' },
+    { type: 'accessibility', name: 'Accessible bathroom', imgUrl: 'https://res.cloudinary.com/dfacuc12l/image/upload/free-parking.svg' },
+    { type: 'accessibility', name: 'Grab rails', imgUrl: 'https://res.cloudinary.com/dfacuc12l/image/upload/free-parking.svg' },
 
-    { type: 'laundry', name: 'Washing machine', imgUrl: '/src/assets/imgs/more/washer.svg' },
-    { type: 'laundry', name: 'Dryer', imgUrl: '/src/assets/imgs/more/dryer.svg' },
-    { type: 'laundry', name: 'Iron', imgUrl: '/src/assets/imgs/more/iron.svg' },
-    { type: 'laundry', name: 'Ironing board', imgUrl: '/src/assets/imgs/more/iron.svg' },
-    { type: 'laundry', name: 'Laundry detergent', imgUrl: '/src/assets/imgs/more/washer.svg' },
+    { type: 'laundry', name: 'Washing machine', imgUrl: 'https://res.cloudinary.com/dfacuc12l/image/upload/washer.svg' },
+    { type: 'laundry', name: 'Dryer', imgUrl: 'https://res.cloudinary.com/dfacuc12l/image/upload/dryer.svg' },
+    { type: 'laundry', name: 'Iron', imgUrl: 'https://res.cloudinary.com/dfacuc12l/image/upload/iron.svg' },
+    { type: 'laundry', name: 'Ironing board', imgUrl: 'https://res.cloudinary.com/dfacuc12l/image/upload/iron.svg' },
+    { type: 'laundry', name: 'Laundry detergent', imgUrl: 'https://res.cloudinary.com/dfacuc12l/image/upload/washer.svg' },
 
-    { type: 'familyFeatures', name: 'Crib', imgUrl: '/src/assets/imgs/Extra/wifi.png' },
-    { type: 'familyFeatures', name: 'High chair', imgUrl: '/src/assets/imgs/Extra/wifi.png' },
-    { type: 'familyFeatures', name: 'Child safety locks', imgUrl: '/src/assets/imgs/Extra/wifi.png' },
-    { type: 'familyFeatures', name: 'Children’s books and toys', imgUrl: '/src/assets/imgs/more/book.svg' },
-    { type: 'familyFeatures', name: 'Baby monitor', imgUrl: '/src/assets/imgs/Extra/wifi.png' },
+    { type: 'familyFeatures', name: 'Crib', imgUrl: 'https://res.cloudinary.com/dfacuc12l/image/upload/wifi.png' },
+    { type: 'familyFeatures', name: 'High chair', imgUrl: 'https://res.cloudinary.com/dfacuc12l/image/upload/wifi.png' },
+    { type: 'familyFeatures', name: 'Child safety locks', imgUrl: 'https://res.cloudinary.com/dfacuc12l/image/upload/wifi.png' },
+    { type: 'familyFeatures', name: 'Children’s books and toys', imgUrl: 'https://res.cloudinary.com/dfacuc12l/image/upload/book.svg' },
+    { type: 'familyFeatures', name: 'Baby monitor', imgUrl: 'https://res.cloudinary.com/dfacuc12l/image/upload/wifi.png' },
 
-    { type: 'petFriendly', name: 'Pets allowed', imgUrl: '/src/assets/imgs/amenities/no-kitchen.svg' },
-    { type: 'petFriendly', name: 'Pet bowls', imgUrl: '/src/assets/imgs/amenities/no-kitchen.svg' },
-    { type: 'petFriendly', name: 'Pet bed', imgUrl: '/src/assets/imgs/amenities/no-kitchen.svg' },
-    { type: 'petFriendly', name: 'Fenced yard', imgUrl: '/src/assets/imgs/amenities/no-kitchen.svg' },
+    { type: 'petFriendly', name: 'Pets allowed', imgUrl: 'https://res.cloudinary.com/dfacuc12l/image/upload/no-kitchen.svg' },
+    { type: 'petFriendly', name: 'Pet bowls', imgUrl: 'https://res.cloudinary.com/dfacuc12l/image/upload/no-kitchen.svg' },
+    { type: 'petFriendly', name: 'Pet bed', imgUrl: 'https://res.cloudinary.com/dfacuc12l/image/upload/no-kitchen.svg' },
+    { type: 'petFriendly', name: 'Fenced yard', imgUrl: 'https://res.cloudinary.com/dfacuc12l/image/upload/no-kitchen.svg' },
 
     // Parking
-    { type: 'parking', name: 'Free parking on premises', imgUrl: '/src/assets/imgs/more/parking.svg' },
-    { type: 'parking', name: 'Street parking', imgUrl: '/src/assets/imgs/amenities/paid-parking.svg' },
-    { type: 'parking', name: 'Garage', imgUrl: '/src/assets/imgs/more/parking.svg' },
-    { type: 'parking', name: 'EV charger', imgUrl: '/src/assets/imgs/more/charger.svg' },
+    { type: 'parking', name: 'Free parking on premises', imgUrl: 'https://res.cloudinary.com/dfacuc12l/image/upload/parking2.svg' },
+    { type: 'parking', name: 'Street parking', imgUrl: 'https://res.cloudinary.com/dfacuc12l/image/upload/paid-parking.svg' },
+    { type: 'parking', name: 'Garage', imgUrl: 'https://res.cloudinary.com/dfacuc12l/image/upload/parking2.svg' },
+    { type: 'parking', name: 'EV charger', imgUrl: 'https://res.cloudinary.com/dfacuc12l/image/upload/charger.svg' },
 
     // Entertainment
-    { type: 'entertainment', name: 'Games', imgUrl: '/src/assets/imgs/amenities/gym.svg' },
-    { type: 'entertainment', name: 'Board games', imgUrl: '/src/assets/imgs/amenities/pool-table.svg' },
-    { type: 'entertainment', name: 'Outdoor toys', imgUrl: '/src/assets/imgs/Extra/pets.png' },
-    { type: 'entertainment', name: 'Books', imgUrl: '/src/assets/imgs/more/book.svg' },
-    { type: 'entertainment', name: 'Music player', imgUrl: '/src/assets/imgs/amenities/piano.svg' }
+    { type: 'entertainment', name: 'Games', imgUrl: 'https://res.cloudinary.com/dfacuc12l/image/upload/gym.svg' },
+    { type: 'entertainment', name: 'Board games', imgUrl: 'https://res.cloudinary.com/dfacuc12l/image/upload/pool-table.svg' },
+    { type: 'entertainment', name: 'Outdoor toys', imgUrl: 'https://res.cloudinary.com/dfacuc12l/image/upload/pets.png' },
+    { type: 'entertainment', name: 'Books', imgUrl: 'https://res.cloudinary.com/dfacuc12l/image/upload/book.svg' },
+    { type: 'entertainment', name: 'Music player', imgUrl: 'https://res.cloudinary.com/dfacuc12l/image/upload/piano.svg' }
 ]
 
 const mainAmenities = [
